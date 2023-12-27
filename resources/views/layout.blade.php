02a1adc007d2c1d1c0e5aa6f725d27d73f0a9d4e
catakeat <catalin.arsulescu@yahoo.com>
2023-12-27 19:50:49 +0200

    SDFD

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
 <header>
@@ -9,7 +9,18 @@
 <div>
     @yield('content')
 </div>
+<div class="content-fluid">
+<div class="row">
+    <div class="col-sm-3">aici
+</div>
 
+<div class="col-sm-3">aici
+</div>
+<div class="col-sm-3">aici
+</div>
+<div class="col-sm-3">aici
+</div>
+</div>
 
 
 
